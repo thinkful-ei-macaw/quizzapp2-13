13e742a6ebdfd40dd2625277280ba199c779d6a0
James Souksavanh Moua <Smoua1558@gmail.com>
2020-02-14 08:53:12 -0500

    Implemented a showQuestion function

--- app.js
+++ app.js
@@ -77,62 +77,139 @@ const store = {
 
  //loads start page
 const start = `
- <section class="start">
-   <h1>Math Quiz</h1>
-   <h2>Are you smarter than a 1st grader?</h2>
+  <section class="start">
+    <h1>Math Quiz</h1>
+    <h2>Are you smarter than a 1st grader?</h2>
 
-   <form id="start-button">
-     <input type="submit" value="Submit">
-   </form>
- </section>`;
+    <form id="start-button">
+    <input type="submit" value="Submit">
+    </form>
+  </section>
 
- //loads results page
-const result = `
-<section class="results">
-  <h2>Summary Screen</h2>
-  <p>Congrats you scored x out of y correct!</p>
-  <form>
-    <input type="submit" value="Restart Quiz">
-  </form>
-</section>   
-`;
+  <section hidden class="quiz">
+    <form>
+      <fieldset>
+        <legend class="question">Question</legend>
+        <input type="radio" name="choice" id="choice-1" value="1">
+        <label for="choice-1">choice</label>
+        <br>
+        <input type="radio" name="choice" id="choice-2" value="2">
+        <label for="choice-2">choice</label>
+        <br>
+        <input type="radio" name="choice" id="choice-3" value="3">
+        <label for="choice-3">choice</label>
+        <br>
+        <input type="radio" name="choice" id="choice-4" value="4">
+        <label for="choice-4">choice</label>
+        <br>
+        <input type="radio" name="choice" id="choice-5" value="5">
+        <label for="choice-5">choice</label>
+        <br>
+      </fieldset>
+      <button type="Submit">Submit</button>
+    </form>
+  </section>
 
-//loads quiz
-const quiz = `
-<section class="quiz">
-      <form>
-        <fieldset>
-          <br>
-        </fieldset>
-      </form>
+  <section hidden class="results">
+    <h2>Summary Screen</h2>
+    <p>Congrats you scored x out of y correct!</p>
+    <form>
+      <input type="submit" value="Restart Quiz">
+    </form>
   </section>
   `;
 
-function run() {
-  const page = start;
-  $('main').html(page);
 
-  $('.start .start-button').click(function(e){
+function generateLegend() {
+  console.log('cool');
+}
+
+function generateChoice() {
+  console.log('rad');
+}
+
+
+function generatePageElement(item){
+
+  const legend = generateLegend();
+  const choice = generateChoice();
+
+  return `
+  <section class="quiz">
+        <form>
+          <fieldset>
+            <legend>${legend}</legend>
+            <input type="radio" value="${choice}">
+            <br>
+          </fieldset>
+        </form>
+  </section>
+    `;
+}
+
+
+function generatePageString(questionAmount) {
+  const pages = store.questions.map((page) => generatePageElement(page));
+
+  return pages.join("");
+}
+
+//renders Questions
+function renderQuestions(){
+  console.log('run');
+}
+
+function renderFirst(){
+  $('main').html(start);
+}
+
+//runs entire scheme
+function renderStart() {
+  //run start
+  renderFirst();
+
+  $('main #start-button').click(function(e){
     e.preventDefault();
-    $('.start').hide();
-    $('.quiz').show();
+
+    $('main .start').attr('hidden', 'hidden');
+    $('main .quiz').removeAttr('hidden');
+
     showQuestion();
-  })
+
+    $('.quiz ul').on()
+  });
 }
 
 function showQuestion() {
-  let theQ = store.question[questionNumber];
-  $('.quiz h2').text(theQ.question);
-  $('.quiz ul').html('');
-  for(let i=0; i < theQ.answers.length; i++){
-    $('.quiz ul').html
-  }
+  let storeQ = store.questions;
+  let qNum = store.questionNumber;
+  let theQ = storeQ[qNum];
+  $('.quiz legend[class="question"]').html(theQ.question);
+  $('.quiz fieldset').html('');
 
+  let numL = Object.values(storeQ)[qNum];
+  let numL2 = numL.answers;
+  for(let i=0; i < numL2.length; i++) {
+    if (i === 0) {
+      $('.quiz fieldset').html($('.quiz fieldset').html() + `
+    <legend class="question">Question: ${qNum} of ${Object.values(storeQ).length}<br>${theQ.question}</legend>
+    <input type="radio" name="choice" id="choice-${i}" value="${numL2[i]}">
+    <label for="choice-${i}">${numL2[i]}</label>
+    <br>
+    `);
+    } else {
+    $('.quiz fieldset').html($('.quiz fieldset').html() + `
+    <input type="radio" name="choice" id="choice-${i}" value="${numL2[i]}">
+    <label for="choice-${i}">${numL2[i]}</label>
+    <br>
+  `)};
+
+  }
 }
 
 //handles important functions
 function handleQuiz(){
-
+  renderStart();
 }
 
 $(handleQuiz);
